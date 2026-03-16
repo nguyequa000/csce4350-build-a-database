8d5ccce87669a4467e2ab17014bb777128a6b563
nguyequa000 <quannguyen4675@gmail.com>
2026-03-15 21:03:14 -0500

    creating parse for set and get and errors handling

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 def run_cli():
-    pass
+    while True:
+        try: 
+            command = input().strip() # Read user input and remove leading/trailing whitespace
+        except EOFError:
+            break
+        if command == "exit":
+            break
+        
+        if command.startswith("SET"):
+            parts = command.split()
+            if len(parts) == 3:
+                print("OK")
+            else:
+                print("ERROR")
+        
+        elif command.startswith("GET"):
+            parts = command.split(" ", 1) # Split into at most 2 parts
+            if len(parts) == 2:
+                print("NULL")
+            else:
+                print("ERROR")
+        else:
+            print("ERROR")
 
 if __name__ == "__main__":
     run_cli()
+    
